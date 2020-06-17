129597bd0fff06ed193a6cf958db4475a2274129
Eng-Yuri89 <m.hitler.kgb89@gmail.com>
2020-06-17 06:20:35 +0300

    add Ticket & Ticket Type

--- app/User.php
+++ app/User.php
@@ -2,7 +2,6 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -15,10 +14,10 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable =[
-        'first_name','last_name', 'email',
-        'mobile','email_verified','mobile_verified',
-        'password', 'shipping_address','billing_address',
+    protected $fillable = [
+        'first_name', 'last_name', 'email',
+        'mobile', 'email_verified', 'mobile_verified',
+        'password', 'shipping_address', 'billing_address',
     ];
 
     /**
@@ -39,32 +38,44 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function orders(){
+    public function orders()
+    {
         return $this->hasMany(Order::class);
     }
 
-    public function Payments(){
+    public function Payments()
+    {
         return $this->hasMany(Payment::class);
     }
 
-    public function shipments(){
+    public function shipments()
+    {
         return $this->hasMany(Shipment::class);
     }
 
-    public function shippingAddress(){
-        return $this->hasOne(Address::class , 'id' ,'shipping_address' );
+    public function shippingAddress()
+    {
+        return $this->hasOne(Address::class, 'id', 'shipping_address');
     }
 
-    public function billingAddress(){
-        return $this->hasOne(Address::class , 'id' ,'billing_address' );
+    public function billingAddress()
+    {
+        return $this->hasOne(Address::class, 'id', 'billing_address');
     }
 
 
-    public function wishList(){
+    public function wishList()
+    {
         return $this->hasOne(WishList::class);
     }
 
-    public function reviews(){
-        return  $this->hasMany(Review::class);
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\City;
+use App\Tag;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +18,44 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('test_email' , function (){
-    return 'Hello';
+    return 'Hello email';
 })->middleware(['auth' , 'email_verified']);
+
+
+Route::get('super_admin' , function (){
+    return 'Hello supper admin';
+})->middleware(['auth' , 'email_verified','user_is_admin','user_is_support']);
+
+Route::get('test_admin' , function (){
+    return 'Hello admin';
+})->middleware(['auth' , 'email_verified','user_is_admin']);
+
+
+Route::get('test_support' , function (){
+    return 'Hello support';
+})->middleware(['auth' , 'email_verified','user_is_support']);
+
+
+Route::get('role-user', function () {
+    $role=\App\Role::find(1);
+    return $role->users;   });
+
+Route::get('user-role', function () {
+    $user=\App\User::find(1);
+    return $user->roles;
+});
+
+
+Route::get('tag-test', function () {
+    $tag=Tag::find(1);
+    return $tag->products;
+});
+
+Route::get('ptag-test', function () {
+    $product=\App\Product::find(2);
+    return $product->tags;
+});
+
 Route::get('states', function () {
     return \App\State::with(['country' , 'cities'])->paginate(1);
 });
