f10c1c9abf18dda630896c47c8f2a7bbaea7047e
Eng-Yuri89 <m.hitler.kgb89@gmail.com>
2020-06-22 02:24:55 +0300

    add Bootstrap MODAL

--- resources/views/admin/units/units.blade.php
+++ resources/views/admin/units/units.blade.php
@@ -31,16 +31,10 @@
                         @foreach($units as $unit)
                             <div class="col-md-3">
                                 <div class="alert alert-primary" role="alert">
-                                    <span>
-                                        <form action="{{route('units')}}"method="post" style="position: relative">
-                                              @csrf
-                                            <input type="hidden" name="_method" value="delete" />
-                                            <input type="hidden" name="unit_id" value="{{$unit->id}}">
-                                            <button type="submit" class="delete_btn"><i class="far fa-trash-alt"></i></button>
-
-                                        </form>
-
-                                    </span>
+                                   <span class="button-span">
+                                        <span><a  class="edit-unit" href=""><i class="fas fa-edit"></i></a></span>
+                                    <span><a  class="delete-unit" href=""><i class="far fa-trash-alt"></i></a></span>
+                                   </span>
                                     <p>{{$unit->unit_name}}, {{$unit->unit_code}}</p>
                                 </div>
                             </div>
@@ -55,20 +49,81 @@
     </div>
 
 
-    <div class="toast" data-autohide="false">
-        <div class="toast-header">
-            <svg class=" rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg"
-                 preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
-                <rect fill="#007aff" width="100%" height="100%"/>
-            </svg>
-            <strong class="mr-auto">Bootstrap</strong>
-            <small class="text-muted">11 mins ago</small>
-            <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>
+
+
+{{--    ------------SPAN DELETE EDIT Boutton--}}
+    <span>
+        <form action="{{route('units')}}"method="post">
+              @csrf
+            <input type="hidden" name="_method" value="delete" />
+            <input type="hidden" name="unit_id" value="{{$unit->id}}">
+            <button type="submit" class="delete_btn"><i class="far fa-trash-alt"></i></button>
+
+        </form>
+
+    </span>
+
+    {{--    ------------BOOTSTRAB MODAL --}}
+    <div class="modal edit-window" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
         </div>
-        <div class="toast-body">
+    </div>
+
+
+{{--    ---------------------------delte------------}}
+    <div class="modal delete-window" tabindex="-1" role="dialog" id="delete-window">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+{{--        -------END BOOTSTRAP MODAL --------------}}
+
+
+
             @if (Session::has('message'))
+                <div class="toast" data-autohide="false">
+                    <div class="toast-header">
+                        <svg class=" rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg"
+                             preserveAspectRatio="xMidYMid slice" focusable="false" role="img">
+                            <rect fill="#007aff" width="100%" height="100%"/>
+                        </svg>
+                        <strong class="mr-auto">Bootstrap</strong>
+                        <small class="text-muted">11 mins ago</small>
+                        <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                <div class="toast-body">
                 {{Session::get('message')}}
 
             @endif
@@ -77,18 +132,42 @@
     </div>
 
 @endsection
-@if (Session::has('message'))
+
+
+
+
+{{--   -------------IF SEECION $ SCRIPT ________________--}}
+
+
+
 @section('scripts')
 
+    <script>
+        $(document).ready(function () {
+            var $deleteUnit = $('.delete-unit');
+            var $deleteWindow = $('#delete-window')
+            $deleteUnit.on('click' , function (element) {
+                element.preventDefault();
+                $deleteWindow.modal('show');
+
+            });
+
+
+        }) ;
+
+    </script>
+
+
+    @if (Session::has('message'))
     <script>
         $(document).ready(function ($) {
             $('.toast').toast('show');
         });
     </script>
-
+    @endif
 @endsection
 
-@endif
+
 
 
 {{--@section('scripts')--}}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
 
-    <script>-*+-</script>
+
     <!-- Scripts -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
